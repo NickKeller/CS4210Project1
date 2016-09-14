ebab160a4a92bd9cb13ddd91b2261ead06c210d4
Nick Keller <nick.keller57@gmail.com>
2016-09-14 00:59:44 -0400

    Can interact with hypervisor to get domain and vm info

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -2,6 +2,73 @@
 #include <stdio.h>
 #include <libvirt/libvirt.h>
 
+#define DEBUG 1
+
+void error(char* message);
+void printUCharAsBinary(unsigned char n);
+
 int main(int argc, char* argv[]){
-	printf("Hello World!!\n");
+
+	//make sure there is one argument
+	if(argc != 2){
+		error("USAGE: ./vcpu_scheduler interval\n");
+	}
+	int interval = atoi(argv[1]);
+	if(DEBUG){
+		printf("Interval is: %d\n",interval);
+	}
+	
+	//connect to the hypervisor
+	virConnectPtr hypervisorConnection = calloc(1, sizeof(virConnectPtr));
+	hypervisorConnection = virConnectOpen("qemu:///system");
+	if(hypervisorConnection != NULL){
+		if(DEBUG){
+			printf("Successful connection to hypervisor!\n");
+		}
+	}
+	else{
+		error("ERROR, could not connect to the hypervisor!\n");
+	}
+	
+	//list all running VMs on the system. Keyword: RUNNING
+	//using listAllDomains, to allow for a variable amount of VMs running
+	unsigned int flags = VIR_CONNECT_LIST_DOMAINS_RUNNING;
+	virDomainPtr* domains = calloc(1, sizeof(virDomainPtr));
+	//this returns a list of virDomainPtrs, can use virDomainGetID to get id of specific one
+	int numDomains = virConnectListAllDomains(hypervisorConnection, &domains, flags);
+	if(numDomains <= 0){
+		//bad
+		error("ERROR, returned 0 or less domains\n");
+	}
+	
+	if(DEBUG)
+		printf("There are %d domains running\n",numDomains);
+	
+	
+	//get the cpumaps of vcpu to pcpu for each domain
+	unsigned char* cpumap = calloc(1, sizeof(unsigned char));
+	//can use numDomains for ncpumaps because there's only 1 vcpu per domain
+	int numVcpus = virDomainGetVcpuPinInfo(domains[0],numDomains,cpumap,sizeof(char),VIR_DOMAIN_AFFECT_LIVE);
+	printf("Num VCPUs:%d\n",numVcpus);
+	printUCharAsBinary(cpumap[0]);
+	
+	//last thing, finish up and close the connection
+	virConnectClose(hypervisorConnection);
+}
+
+void printUCharAsBinary(unsigned char n){
+	while (n) {
+    if (n & 1)
+        printf("1");
+    else
+        printf("0");
+
+    n >>= 1;
+}
+printf("\n");
+}
+
+void error(char* message){
+	printf("%s",message);
+	exit(-1);
 }
--- memory/memory_coordinator.c
+++ memory/memory_coordinator.c
@@ -2,6 +2,127 @@
 #include <stdio.h>
 #include <libvirt/libvirt.h>
 
+#define DEBUG 1
+
+void error(char* message);
+
 int main (int argc, char* argv[]){
-	printf("Hello World!!\n");
+	//make sure there is one argument
+	if(argc != 2){
+		error("USAGE: ./memory_coordinator interval\n");
+	}
+	int interval = atoi(argv[1]);
+	if(DEBUG){
+		printf("Interval is: %d\n",interval);
+	}
+	
+	//connect to the hypervisor
+	virConnectPtr hypervisorConnection = calloc(1, sizeof(virConnectPtr));
+	hypervisorConnection = virConnectOpen("qemu:///system");
+	if(hypervisorConnection != NULL){
+		if(DEBUG){
+			printf("Successful connection to hypervisor!\n");
+		}
+	}
+	else{
+		error("ERROR, could not connect to the hypervisor!\n");
+	}
+	
+	//list all running VMs on the system. Keyword: RUNNING
+	//using listAllDomains, to allow for a variable amount of VMs running
+	unsigned int flags = VIR_CONNECT_LIST_DOMAINS_RUNNING;
+	virDomainPtr* domains = calloc(1, sizeof(virDomainPtr));
+	//this returns a list of virDomainPtrs, can use virDomainGetID to get id of specific one
+	int numDomains = virConnectListAllDomains(hypervisorConnection, &domains, flags);
+	if(numDomains <= 0){
+		//bad
+		error("ERROR, returned 0 or less domains\n");
+	}
+	
+	if(DEBUG)
+		printf("There are %d domains running\n",numDomains);
+	
+	for(int i = 0; i < numDomains; i++){
+		virDomainPtr curDomain = domains[i];
+		virDomainSetMemoryStatsPeriod(
+		if(!curDomain){
+			printf("Domain at %d is null\n",i);
+			exit(-1);
+		}
+		int domainID = virDomainGetID(curDomain);
+		printf("Domain ID is %d\n",domainID);
+		//get all memory stats for this domain
+		//max memory available
+		printf("Getting max mem available\n");
+		//unsigned long maxMemAvailable = virDomainGetMaxMemory(curDomain);
+		
+		virDomainMemoryStatPtr stats = calloc(1, sizeof(virDomainMemoryStatPtr));
+		//memory stats
+		printf("Getting memory stats\n");
+		int numStats = virDomainMemoryStats(curDomain, stats, 9, 0);
+		printf("Returned %d stats\n", numStats);
+		for(int j = 0; j < numStats; j++){
+			printf("TAG:%d\n",stats[j].tag);
+		}
+		
+		//printf("Max Mem available: %lu\n", maxMemAvailable);
+	}
+	
+	
+	//last thing, free the hypervisor connection
+	virConnectClose(hypervisorConnection);
 }
+
+
+void error(char* message){
+	printf("%s",message);
+	exit(-1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
